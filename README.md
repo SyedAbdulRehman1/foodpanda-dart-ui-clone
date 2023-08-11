22300c271c7c7c6f6d9c1ebe5cb1ee4452414ef1
Hafiz Syed Abdul Rehman <syedAbdulRehman1>
2023-08-11 02:38:32 -0700

    foodpanda ui

---
+++ README.md
@@ -0,0 +1,12 @@
+# Foodpand UI Clone
+
+
+- [Youtube](https://youtu.be/bCS1ddMT_sU)
+- [LinkedIn](https://www.linkedin.com/in/axiftaj/)
+- [Facebook Group](https://www.facebook.com/aaxiftaj)
+
+
+![new](https://user-images.githubusercontent.com/47206155/161398166-7d2af7f3-490f-4f1c-ae9b-be288597f25e.png)
+
+
+![food panda thumnail](https://user-images.githubusercontent.com/47206155/161398035-b2531a0a-1a44-4bc4-b422-041cdd699824.png)
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/constats/color.dart';
+import 'package:instagram_clone/constats/constants.dart';
+import 'package:instagram_clone/dashboard/splash_screen.dart';
+// import 'package:instagram_clone/dashboard/welcome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: MyColors.navy,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: const Color(0xfffcfcfc),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: .2,
+          titleTextStyle: TextStyle(fontFamily: Bold, color: Color(0xff323232)),
+          iconTheme: IconThemeData(color: MyColors.primaryColor),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: MyColors.navy,
+        scaffoldBackgroundColor: const Color(0xfffcfcfc),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: .2,
+          titleTextStyle: TextStyle(fontFamily: Bold, color: Color(0xff323232)),
+          iconTheme: IconThemeData(color: MyColors.primaryColor),
+        ),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
